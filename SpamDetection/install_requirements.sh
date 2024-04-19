32d298fef3fafb6add4816780e9ff08d03f7f359
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-04-18 23:24:44 -0400

    Initial dataset

---
+++ SpamDetection/install_requirements.sh
@@ -0,0 +1,3 @@
+pip install pandas matplotlib numpy pandas nltk  "tf-models-official==2.14.*" tensorflow[and-cuda] seaborn gdown tokenizers transformers
+pip uninstall tensorflow==2.13.1
+pip install -U "tensorflow-text==2.14.*"
